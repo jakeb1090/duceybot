3585a0e29cd41d3cc7948a753e9e4b9944c6ba1b
jakeb1090 <69651391+jakeb1090@users.noreply.github.com>
2020-12-21 21:29:24 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,24 @@ Base.metadata.create_all(bind=engine)
 def bot():
     if request.method == 'GET':
         t_models.get_tweets()
-        return {
-            "status": "retweets okay",
+#         return {
+#             "status": "retweets okay",
 
-            }
+#             }
+        print('doing manual check')        
+        print({'status': 'checking walmart'})
+        time.sleep(3)
+        print({'status': 'checking best buy'})
+        time.sleep(3)
+        print({'status': 'checking target'})
+        time.sleep(3)
+        print({'inventory status': 'sold out'})
+        time.sleep(1)
+        print({'message type': 'text'})
+        time.sleep(1)
+        print({'text mae': false})
+    
+    
     else:
         title = request.form['title']
         poster_url = m.build_imgurl(title)
@@ -26,18 +40,7 @@ def bot():
         if r.status_code != 200:
             message = "No match"
             return {"status": "no retweets"}
-#             print('doing manual check')        
-#         print({'status': 'checking walmart'})
-#         time.sleep(3)
-#         print({'status': 'checking best buy'})
-#         time.sleep(3)
-#         print({'status': 'checking target'})
-#         time.sleep(3)
-#         print({'inventory status': 'sold out'})
-#         time.sleep(1)
-#         print({'message type': 'text'})
-#         time.sleep(1)
-#         print({'text mae': false})
+
         # return render_template("photo.html", image=poster_url)
 
         
